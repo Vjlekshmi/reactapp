4da998331ec15ce065064d76c419b54bded53358
vjlekshmi <achu191998@gamil.com>
2022-05-17 10:40:15 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Addstudent from './components/Student';
+import Search from './components/Search';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  return (
+    <><Addstudent/><Search/></>
+  );
+}
+
+export default App;
